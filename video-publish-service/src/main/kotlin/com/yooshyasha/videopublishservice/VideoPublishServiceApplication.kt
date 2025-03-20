0439d2240f9e772b2223f4859a4ded82f42b6a8e
Yooshyasha <mejzikrejzi66@gmail.com>
2025-03-20 20:05:34 +0500

    video publish service initial commit

---
+++ video-publish-service/src/main/kotlin/com/yooshyasha/videopublishservice/VideoPublishServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.yooshyasha.videopublishservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VideoPublishServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<VideoPublishServiceApplication>(*args)
+}
